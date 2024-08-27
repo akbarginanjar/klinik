44baa1aabf6e74e2e3a7a723b5fc3f84f22e9867
akbarginanjar <akbarginanjarr0@gmail.com>
2024-08-27 16:20:46 +0700

    galeri

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -6,8 +6,8 @@
 
         <!-- Section Title -->
         <div class="container section-title" data-aos="fade-up">
-            <h2>Gallery</h2>
-            <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+            <h2>Galeri</h2>
+            <p>Klinik Pratama Hegar</p>
         </div><!-- End Section Title -->
 
         <div class="container-fluid" data-aos="fade-up" data-aos-delay="100">
